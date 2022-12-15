34ff30f06b5abb416d2658210381a160c34e49c7
shimy8617 <cristinashim@yahoo.com.ar>
2022-12-15 10:44:22 -0300

    Add task mock and interface

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit{
   @Input() text: string = "";
   @Input() color: string = "";
   @Output() btnClick = new EventEmitter();
